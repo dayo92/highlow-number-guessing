f018396cb4d41441de48c687700a684b4d6e95c3
Daniel A <dan.ataiyero@hotmail.com>
2024-02-08 00:55:25 +0000

    Fix #4  removed unnecessarily decrease from loop

--- highlow-number-guessing/Program.cs
+++ highlow-number-guessing/Program.cs
@@ -7,6 +7,7 @@ namespace highlow_number_guessing
         public static void Main(string[] args)
         {
             int maxGuess = 5;
+            int leftGuess;
             
             Console.WriteLine("Welcome to the High/Low number guessing game");
             Console.WriteLine($"The rules are you have a total of {maxGuess} guesses to find the correct random number between 1 and 100.");
@@ -14,7 +15,7 @@ namespace highlow_number_guessing
             Random random = new Random();
             int randomNumber = random.Next(1, 101);
             
-            for (int i = maxGuess; i > 0; i--)
+            for (leftGuess = maxGuess; leftGuess > 0;)
             {
                 Console.Write("Enter your guess: ");
                 string guessedNumber = Console.ReadLine();
@@ -22,28 +23,26 @@ namespace highlow_number_guessing
                 if(!int.TryParse(guessedNumber, out int playerNumber))
                 {
                     Console.WriteLine("Please use NUMBERS not LETTERS.");
-                    i++; 
                     continue;
                 }
 
                 if (playerNumber < randomNumber)
                 {
                     Console.WriteLine("Too low.");
-                    maxGuess--;
                 }
                 if (playerNumber > randomNumber)
                 {
                     Console.WriteLine("Too high.");
-                    maxGuess--;
                 }
                 if (playerNumber == randomNumber)
                 {
                     Console.WriteLine("Congratulations! You guessed the secret number.");
-
                 }
-                Console.WriteLine("number of guesses left: "+maxGuess);
+
+                leftGuess--;
+                Console.WriteLine("number of guesses left: " + leftGuess);
             }
-            if (maxGuess == 0)
+            if (leftGuess == 0)
             {
                 Console.WriteLine("GAME OVER!!! You have no guesses left");
                 Console.WriteLine($"The random number was {randomNumber}.");
